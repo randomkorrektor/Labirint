707941fcc6bbec1efe66eeed3055b7916142af24
lochid <pmi22dremov@gmail.com>
2016-10-23 18:00:01 +0700

    Start create labyrinth manager.

---
+++ Labirint/ClientManagers/LabyrinthManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManagers
+{
+    public struct Pos
+    {
+        public int x;
+        public int y;
+    }
+    public class LabyrinthManager
+    {
+        public static LabyrinthManager Instance = new LabyrinthManager();
+
+        private LabyrinthManager() { }
+
+        public Queue<Pos> queue;
+
+        public bool[,]  getCell()
+        {
+            return null;
+        }
+
+        public void find()
+        {
+
+        }
+    }
+}
